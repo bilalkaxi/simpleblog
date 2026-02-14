bb0f692f044c461797d369b7140adc7c52e0eeb8
bilalkaxi <bilaljamiljtu@gmail.com>
2026-02-14 21:40:28 +0500

    added database + prisma@6

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { PrismaClient } = require('@prisma/client');
 const app = express();
+const prisma = new PrismaClient();
 const port = 3000;
 //midle ware
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//in memory blog
-let posts = [
-    {
-        title: "Hello",
-        content: "WOrld"
-    },
-]
 
-app.get('/', (req, res) => {
-    res.render('index', { posts: posts });
+//routes
+app.get('/', async (req, res) => {
+    const posts = await prisma.post.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    // const postsWithDate = posts.map(post => ({
+    //     ...post,
+    //     createdAt: new
+    // }))
+    res.render('index', { posts });
 });
+
 app.get('/compose', (req, res) => {
     res.render('compose');
 });
-app.post('/compose', (req, res) => {
-    const post = {
-        title: req.body.title,
-        content: req.body.content
-    };
-    posts.push(post);
+app.post('/compose', async (req, res) => {
+    const { title, content } = req.body;
+    await prisma.post.create({
+        data: {
+            title,
+            content
+        }
+    })
     res.redirect('/');
 });
 
